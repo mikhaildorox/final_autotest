c461f94f78b3d277ff6c70d7cfa4170ef62eb031
mikhaildoroshenko <mixdorosh@gmail.com>
2023-06-29 00:07:53 +0700

    lesson 4.1 step 6 write test login url(failed),test login form(passed), register form(passed)

--- test_main_page.py
+++ test_main_page.py
@@ -21,3 +21,17 @@ def test_guest_should_see_login_url(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
