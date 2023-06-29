fcc1796583a19905780e9bcbe84612b793f3804c
mikhaildoroshenko <mixdorosh@gmail.com>
2023-06-30 02:30:51 +0700

    lesson 4.1 step 6 write test login url(passed) -login_url in locator, fix should_be_login_url

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,5 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-    LOGIN_URL = (By.LINK_TEXT, 'http://selenium1py.pythonanywhere.com/ru/accounts/login/')
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
